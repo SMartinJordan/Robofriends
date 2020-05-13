83ed296f547c9c2722d861c40b571e4e8adbf757
Jordan Mbeleg <jordanmbeleg@live.fr>
2020-05-13 19:23:22 -0400

    Updates

---
+++ precache-manifest.cc18c9c80e8ebfc0ef23093af939178c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d76ae03f42a53e83cf43ef545f5a1aaa",
+    "url": "/Robotfriends/index.html"
+  },
+  {
+    "revision": "ed9b6b7b3f5e1db31da5",
+    "url": "/Robotfriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4315a4ae66239655bae1",
+    "url": "/Robotfriends/static/css/main.a4cb19a4.chunk.css"
+  },
+  {
+    "revision": "ed9b6b7b3f5e1db31da5",
+    "url": "/Robotfriends/static/js/2.12e19136.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robotfriends/static/js/2.12e19136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4315a4ae66239655bae1",
+    "url": "/Robotfriends/static/js/main.0dcea923.chunk.js"
+  },
+  {
+    "revision": "053419f7f0af2c8832f7",
+    "url": "/Robotfriends/static/js/runtime-main.6a719897.js"
+  }
+]);
